acd6b01eeebf8d6e556ead04733053a673a64f2f
DanielleClifford <bethebarnabas@gmail.com>
2023-11-30 08:56:39 -0600

    fixed loader

--- js/index.js
+++ js/index.js
@@ -1,14 +1,7 @@
 // adding javascript to LibbyKids Page 1
-document.onreadystatechange = function () {
-    if (document.readyState !== "complete") {
-        document.querySelector(
-            "body").style.visibility = "hidden";
-        document.querySelector(
-            "#loader").style.visibility = "visible";
-    } else {
-        document.querySelector(
-            "#loader").style.display = "none";
-        document.querySelector(
-            "body").style.visibility = "visible";
+
+function fade() {
+    $('.preloader').fadeOut("slow");
     }
-};
+    setTimeout(fade, 3000);
+    
